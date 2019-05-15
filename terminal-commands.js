274281f07423f057327845f35a6b37377ec4f850
Arturo Rivera <a.rivera13@hotmail.com>
2019-05-14 20:38:06 -0700

    Implemented touch and mkdir

--- terminal-commands.js
+++ terminal-commands.js
@@ -3,8 +3,22 @@ const fs = require('fs');
 module.exports.ls = () => {
     fs.readdir('./', (err, files) => {
       const filesToString = files.reduce((acc, file) => {
-        return 'Hello ${acc} ${file}';
+        return '${acc} ${file}';
       }, '');
       console.log(filesToString);
     });
+};
+
+module.exports.touch = () => {
+  fs.writeFile('terminal-commands.js', (err) => {
+    if (err) throw err;
+    console.log('File Created');
+  })
+};
+
+module.exports.mkdir = () => {
+  fs.mkdir('./terminal-commands', function(err) => {
+    if (err) throw err;
+    console.log('Directory Created');
+  })
 };
